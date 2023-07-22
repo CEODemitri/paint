43d960bf413fc7640f6e6cd8dd21b34227867330
Color Codex <colorcodex8@Colors-iMac.local>
2023-07-22 03:13:22 -0500

    broke down homepage into components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import NavBar from "./components/NavBar";
+import Card from "./components/Card";
 import "./App.css";
 
 function App() {
@@ -8,19 +9,7 @@ function App() {
       <div className="container text-center mt-5">
         <h1>Gallery</h1>
         <div className="row">
-          {Array.apply(null, { length: 9 }).map(() => {
-            return (
-              <div className="col mb-5"> 
-                <div class="card" style={{ width: "18rem" }}>
-                  <img
-                    src="https://via.placeholder.com/200"
-                    class="card-img-top"
-                    alt="not my work"
-                  />
-                </div>
-              </div>
-            );
-          })}
+          {Array.apply(null, { length: 9 }).map(() => <Card />)}     
         </div>
       </div>
     </>
