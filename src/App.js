9a126fd1c72f55187ffa05159ebcceb2321ccac0
Color Codex <colorcodex8@Colors-iMac.local>
2023-07-26 00:02:40 -0500

    added more functionality plus some refactoring

--- src/App.js
+++ src/App.js
@@ -1,46 +1,71 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import NavBar from "./components/NavBar";
 import Card from "./components/Card";
 import UploadForm from "./components/UploadForm";
 import "./App.css";
 
-const photos = [
-  'https://picsum.photos/id/1014/200/200',
-  'https://picsum.photos/id/1031/200/200',
-  'https://picsum.photos/id/1038/200/200',
-  'https://picsum.photos/id/1041/200/200',
-  'https://picsum.photos/id/1001/200/200'
-]
+const photos = [];
+
+const initialState = {
+  items: photos,
+  count: photos.length,
+  inputs: { title: null, file: null, path: null },
+  isCollapsed: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setItem":
+      return {
+        ...state,
+        items: [action.payload.path, ...state.items],
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState()
-  const [inputs, setInputs] = useState({ title: null, file: null, path: null});
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [count, setCount] = useState();
+  const [inputs, setInputs] = useState({ title: null, file: null, path: null });
   const [items, setItems] = useState(photos);
   const [isCollapsed, collapse] = useState(false);
   const toggle = () => collapse(!isCollapsed);
   const handleOnChange = (e) => {
-    if (e.target.name === 'file') {
-      setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
+    if (e.target.name === "file") {
+      setInputs({
+        ...inputs,
+        file: e.target.files[0],
+        path: URL.createObjectURL(e.target.files[0]),
+      });
     } else {
-      setInputs({...inputs, title: e.target.value})
+      setInputs({ ...inputs, title: e.target.value });
     }
-   
-  }
+  };
   const handleOnSubmit = (e) => {
-    e.preventDefault()
-    setItems([inputs.path,...items])
-  }
+    e.preventDefault();
+    // setItems([inputs.path, ...items]);
+    dispatch({ type: "setItem", payload: { path: inputs } });
+    setInputs({ title: null, file: null, path: null });
+    collapse(false);
+  };
+
+  useEffect(() => {}, [state.items]);
 
   useEffect(() => {
-    setCount(`There are ${items.length} image${items.length > 1 ? 's': ''}`)
-  }, [items])
+    setCount(`There are ${items.length} image${items.length > 1 ? "s" : ""}`);
+  }, [items]);
   return (
     <>
       <NavBar />
       <div className="container text-center mt-5">
-        <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
+        <button className="btn btn-success float-end" onClick={toggle}>
+          {isCollapsed ? "Close" : "+ Add"}
+        </button>
         <div className="clearfix mb-4"></div>
-        <UploadForm 
+        <UploadForm
+          inputs={inputs}
           isVisible={isCollapsed}
           onChange={handleOnChange}
           onSubmit={handleOnSubmit}
@@ -49,11 +74,12 @@ function App() {
         <h1>Gallery</h1>
 
         <div className="row">
-        {items.map((photo, index) => <Card key={index} src={photo}/>)}
+          {state.items.map((photo, index) => (
+            <Card key={index} src={photo.path} />
+          ))}
         </div>
       </div>
     </>
   );
- 
 }
 export default App;
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,9 +1,36 @@
-const UploadForm = ({ isVisible, onChange, onSubmit }) => {
-    return (
-        isVisible && <>
-        <p className="display-6 text-center mb-3">Upload Stock Image</p>
+import { useMemo } from "react";
+
+const Preview = ({ path }) => {
+  return (
+    path && (
+      <div
+        className="rounded p-1 m-5"
+        style={{
+          width: "30%",
+          height: "300px",
+          backgroundImage: `url(${path}`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+    )
+  );
+};
+
+const UploadForm = ({ inputs, isVisible, onChange, onSubmit }) => {
+  const isDisabled = useMemo(() => {
+    return !!Object.values(inputs).some((input) => !input);
+  }, [inputs]);
+  return (
+    isVisible && (
+      <>
+        <p className="display-6 text-center mb-3">Upload Your Image</p>
         <div className="mb-5 d-flex align-items-center justify-content-center">
-          <form className="mb-2" style={{ textAlign: "left" }} onSubmit={onSubmit}>
+          <Preview {...inputs} />
+          <form
+            className="mb-2"
+            style={{ textAlign: "left" }}
+            onSubmit={onSubmit}
+          >
             <div className="mb-3">
               <input
                 type="text"
@@ -15,18 +42,24 @@ const UploadForm = ({ isVisible, onChange, onSubmit }) => {
               />
             </div>
             <div className="mb-3">
-              <input type="file" className="form-control" name="file" onChange={onChange}/>
+              <input
+                type="file"
+                className="form-control"
+                name="file"
+                onChange={onChange}
+              />
             </div>
             <button
               type="submit"
               className="btn btn-success float-end"
+              disabled={isDisabled}
             >
               Save changes
             </button>
           </form>
         </div>
-        </>
-      );
-   };
-
-  export default UploadForm;
+      </>
+    )
+  );
+};
+export default UploadForm;
